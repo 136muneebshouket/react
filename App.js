48e4a5aa849671f45a04256efd00e11f48025a5f
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-02-10 09:26:01 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import  './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      < Navbar title="textutils" about="about" />
+       
+
+
+
+      
+
+
+
+
+
+    </>
+  );
+}
+
+export default App;
