e081ef6d14e4a457983635438e9f7651d15f6005
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-04-19 14:04:51 -0700

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { useState } from "react";
+
+
+export default function Form() {
+
+    const [name, setName] = useState("");
+    const [Value, setValue] = useState([]);
+    const [Update,setUpdate]= useState(true);
+    const [selecteditem, setselecteditem]= useState(null);
+
+
+    const func = (event) => {
+        event.preventDefault();
+
+       const alldata = {id: new Date().getTime().toString() , data : name}
+        setValue( [...Value, alldata])
+
+        
+
+        setName('');
+    }
+    
+
+
+    const del = (index) => {
+      
+         const newtodos = Value.filter((item) =>{
+             return index != item.id;
+              
+         });
+
+        // const newtodos = [...Value];
+        // newtodos.splice(index, 1);
+        setValue(newtodos);
+
+
+    }
+
+    const edit = (id) => {
+    let selectedvalue = Value.find((item) => {
+        return item.id === id; 
+    })
+    console.log(selectedvalue);
+    setUpdate(false);
+
+      
+        
+    setName(selectedvalue.data);
+    setselecteditem(id);
+       
+    }
+
+    const update = (event) => {
+        event.preventDefault();
+        console.log('update');
+        setValue(Value.map((elem) =>{
+             if(elem.id=== selecteditem){
+                return {...elem, data :name}
+
+             }
+                return elem;
+        }))
+
+        setName('');
+        setUpdate(true);
+
+    }
+
+
+    
+    return (
+        <>
+            <form className='form'>
+                <label>Enter your name:
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
+               { Update ? <button onClick={func}>showit</button> :
+
+                 <button onClick={update}>update</button> 
+               }
+               
+                
+            </form>
+
+            <div>
+                {Value.map((item) => {
+                    return (
+
+                        <div key={item.id}>
+                            <div>{item.data} <button onClick={() => del(item.id)}>delete</button>
+                                <button onClick={() => edit(item.id)}>edit</button>
+
+
+                            </div>
+                        </div>
+
+                    )
+                }
+
+                )}
+
+            </div>
+        </>
+    )
+}
