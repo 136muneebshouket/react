8064c5c1ff778d23d940ca52bd69c1ae1f0d6700
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-05-21 12:25:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 
 import React from 'react'
 // import fire from './component/auth/Auth';
-// import Header from "./component/header/Header";
+import Header from "./component/header/Header";
 // import Signup from "./component/signup/Signup";
-// import Fetch from "./component/fetch/Fetch";
-// import Delete from "./component/delete/Delete";
+import Fetch from "./component/fetch/Fetch";
+import Delete from "./component/delete/Delete";
 // import Auth from "./component/auth/Auth";
-// import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Registe from "./component/REGISTER/Registe";
 function App() {
 
@@ -15,24 +15,25 @@ function App() {
   return (<>
    
    
-    {/* <BrowserRouter>
+   <BrowserRouter>
     <Header/>
     <Routes>
-      <Route element={<Signup/>} path="/"></Route>
+      <Route element={<Registe/>} path="/"></Route>
     </Routes>
-    {/* <Routes>
+   <Routes>
       <Route element={<Delete/>} path="/delete"></Route>
     </Routes>
     <Routes>
       <Route element={<Fetch/>} path="/fetch"></Route>
-    </Routes> */}
-    {/* <Routes>
+    </Routes> 
+     {/* <Routes>
       <Route element={<Auth/>} path="/login"></Route>
-    </Routes>
-    </BrowserRouter>   */} 
-       <Registe/>
+    </Routes> */}
+    </BrowserRouter>   
+       
     </>
   )
  }
 
 export default App
+
